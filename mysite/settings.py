0f541ffc66962cf44ddaf32cac4facaeee81a6ad
Justyna Klijewska <jklijewska@fs.local>
2019-09-08 12:17:08 +0200

    Final version - blog application.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'my=)tjgzai1ahhocg(u8&-x@+0x6@@m(hf)uehen(eot+wo7(0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
